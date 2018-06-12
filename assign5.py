0955436d4955e0d8e1b84c6d7a2acc81ac8a960b
Harshitsavager1 <harshit.8908@gmail.com>
2018-06-13 00:04:08 +0530

    my third commit

---
+++ assign5.py
@@ -0,0 +1,29 @@
+#question_1(tuple)
+tuple_1=("justin timberlake","maroon","selena gomez")
+print(tuple_1)
+print(len(tuple_1))
+print(30*"*")
+#question_2
+print("maximum element in tuple_1",max(tuple_1))
+print("minimum element in tuple_1",min(tuple_1))
+print(30*"*")
+#question_3
+print("question_3:doubt ")
+#question_1(sets)
+#i
+set_1={"5","7","1","4","3"}
+set_2={"8","3","9","6","4"}
+print("1st set",set_1)
+print("2nd set",set_2)
+print("difference of two sets",set_1-set_2)
+print(15*"*")
+#ii
+print("compare:both sets are equal or not (true/false)=",(set_1==set_2))
+print(15*"*")
+#iii
+print("intersection of two sets", set_1&set_2)
+print(30*"*")
+#question_1(dictionary)
+print('''doubt cause of "for" loop''')
+
+
